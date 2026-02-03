5e227ad8f0ecb48c5f85f4b8088c4442554e96cf
mahen112 <mahen112@mail.com>
2026-02-03 12:39:09 +0530

    initial commit for Demo Project

---
+++ src/com/training/Main.java
@@ -0,0 +1,25 @@
+package com.training;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		System.out.println("Hello World!!");
+		String url = "jdbc:postgresql://localhost:5432/testdb";
+		String username = "postgres";
+		String password="pass123";
+		Class.forName("org.postgresql.Driver");
+		Connection conn = DriverManager.getConnection(url,username, password);
+		if(conn != null) {
+			System.out.println("Connected Successfully...");
+		}
+		else {
+			System.out.println("Connection failed..");
+		}
+
+	}
+
+}
